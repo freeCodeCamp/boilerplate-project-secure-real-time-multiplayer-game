802cee2b59d4db65f91a20ce7cc6827d6deac3e1
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2020-06-09 15:00:57 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai');
+const socket = require('socket.io');
+
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner.js');
+
+const app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/assets', express.static(process.cwd() + '/assets'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  }); 
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+    
+// 404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+const portNum = process.env.PORT || 3000;
+
+// Set up server and tests
+const server = app.listen(portNum, () => {
+  console.log(`Listening on port ${portNum}`);
+  if (process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch (error) {
+        console.log('Tests are not valid:');
+        console.error(error);
+      }
+    }, 1500);
+  }
+});
+
+module.exports = app; // For testing
